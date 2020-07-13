a3819e64c823b7b190bc24f274afe151ee805cb3
JotaMarcos <jmarcos_duarte@hotmail.com>
2020-07-13 11:45:37 -0400

    Primeiro Projeto em Angula, com segregação de responsabilidades

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { Error404Component } from './component/error-404/Error404Component';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    NavBarComponent,
+    Error404Component
+  ],
+  imports: [
+    RouterModule.forChild([
+      {
+        path: '**', component: Error404Component
+      }
+    ])
+  ],
+  exports: [
+    NavBarComponent
+  ]
+})
+export class CoreModule {
+}
+
+
+
+
+
+
+
+
+
